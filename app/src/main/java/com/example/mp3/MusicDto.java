e0adf609a258842ca34ce02248b99dd4b4dcb080
rusia9217 <110519493+unnimm@users.noreply.github.com>
2023-06-17 20:16:16 +0900

    mp3 애플리케이션입니다.

---
+++ app/src/main/java/com/example/mp3/MusicDto.java
@@ -0,0 +1,66 @@
+package com.example.mp3;
+
+import java.io.Serializable;
+
+/**
+ * Created by 상일 on 2016-06-19.
+ */
+public class MusicDto implements Serializable {
+    private String id;
+    private String albumId;
+    private String title;
+    private String artist;
+
+    public MusicDto() {
+    }
+
+    public MusicDto(String id, String albumId, String title, String artist) {
+        this.id = id;
+        this.albumId = albumId;
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicDto{" +
+                "id='" + id + '\'' +
+                ", albumId='" + albumId + '\'' +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                '}';
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
+[출처] [Android] 안드로이드 Mp3 플레이어 만들기 / MediaPlayer|작성자 Bitpump
